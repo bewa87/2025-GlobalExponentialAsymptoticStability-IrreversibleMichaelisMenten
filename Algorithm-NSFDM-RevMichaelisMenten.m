bbb333fb81cf6d73c667ff34896dd11cd8a229d2
bewa87 <63112064+bewa87@users.noreply.github.com>
2026-06-01 13:15:37 +0200

    Update Algorithm-NSFDM-RevMichaelisMenten.m

--- Algorithm-NSFDM-RevMichaelisMenten.m
+++ Algorithm-NSFDM-RevMichaelisMenten.m
@@ -1,8 +1,8 @@
 % Step 1: Introduction of Problem Parameters and Initial Conditions
 
-k_01 = 0.15;
-k_02 = 0.10;
-k_03 = 0.05;3
+k_01 = 0.15; %0.15;
+k_02 = 0.10; %0.10;
+k_03 = 0.05; %0.05;
 
 s_0  = 0.5;
 e_0  = 0.2;
@@ -11,8 +11,8 @@ p_0  = 0.2;
 
 % Step 2: Initialization of Time Grid and Solution Components
 
-T    = 1000;
-h    = 25;
+T    = 10000;
+h    = 10;
 t    = (0:h:T)';
 
 s    = zeros(length(t),1);
@@ -41,7 +41,17 @@ endfor
 
 lambda = -k_01*k_03*(e_0+c_0)/(k_01*(e_0+c_0)+k_01*(s_0+c_0+p_0)+(k_02+k_03));
 
-% Step 5: Plotting of Solution Components and Conservation Laws
+% Step 5: Calculation of limit (c/c+s) =: B for t to infinity
+
+A = - ((k_02+k_03-k_01*(e_0+c_0))/(2*k_02)) + sqrt(((k_02+k_03-k_01*(e_0+c_0))/(2*k_02))^2+((k_01*(e_0+c_0))/(k_02)));
+B = A/(1+A);
+
+% Step 5.1: alpha of Bersani 2012
+
+alpha_bersani = (k_01/2)*(((k_02+k_03)/k_01)+(e_0+c_0))*(1-sqrt(1-4*k_03*(e_0+c_0)/(k_01*(((k_02+k_03)/k_01)+(e_0+c_0)))));
+K_W_bersani   = (k_03-alpha_bersani)/alpha_bersani;
+
+% Step 6: Plotting of Solution Components and Conservation Laws
 
 figure(1)
 plot(t,s,'linewidth',0.8,'marker','*','linestyle','-')
@@ -63,62 +73,108 @@ xlim([0 1000])
 ylim([0 0.8])
 hold off
 
-figure(2)
-plot(t,e+c,'linewidth',0.8,'marker','*')
-hold on
-plot(t,s+c+p,'linewidth',0.8,'marker','+')
-title('Conservation laws of enzymatic reaction','fontsize',20)
-legend({'e(t)+c(t)','s(t)+c(t)+p(t)'},'location','eastoutside','fontsize',14)
-xlabel('Time','fontsize',14)
-ylabel('Conservation laws','fontsize',14)
-xlim([0 1000])
-ylim([0 0.8])
-hold off
+##figure(2)
+##plot(t,e+c,'linewidth',0.8,'marker','*')
+##hold on
+##plot(t,s+c+p,'linewidth',0.8,'marker','+')
+##title('Conservation laws of enzymatic reaction','fontsize',20)
+##legend({'e(t)+c(t)','s(t)+c(t)+p(t)'},'location','eastoutside','fontsize',14)
+##xlabel('Time','fontsize',14)
+##ylabel('Conservation laws','fontsize',14)
+##xlim([0 1000])
+##ylim([0 0.8])
+##hold off
 
 figure(3)
 plot(t,s,'linewidth',0.8,'marker','*','linestyle','-')
 hold on
-plot(t,max([s_0 e_0 c_0 p_0]).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
+plot(t,1*(s_0+e_0+c_0+p_0).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
 title('Exponential decay of |s(t) - 0|','fontsize',20)
 legend({'s(t)','exp. decay'},'location','eastoutside','fontsize',14)
 xlabel('Time','fontsize',14)
 ylabel('s(t)','fontsize',14)
 xlim([0 1000])
-ylim([0 0.6])
+ylim([0 0.8])
 hold off
 
 figure(4)
 plot(t,c,'linewidth',0.8,'marker','*','linestyle','-')
 hold on
-plot(t,max([s_0 e_0 c_0 p_0]).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
+plot(t,1*(s_0+e_0+c_0+p_0).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
 title('Exponential decay of |c(t) - 0|','fontsize',20)
 legend({'c(t)','exp. decay'},'location','eastoutside','fontsize',14)
 xlabel('Time','fontsize',14)
 ylabel('c(t)','fontsize',14)
 xlim([0 1000])
-ylim([0 0.6])
+ylim([0 0.8])
 hold off
 
 figure(5)
 plot(t,abs(e-(e_0+c_0)*ones(length(e),1)),'linewidth',0.8,'marker','*','linestyle','-')
 hold on
-plot(t,max([s_0 e_0 c_0 p_0]).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
+plot(t,1*(s_0+e_0+c_0+p_0).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
 title('Exponential decay of |e(t) - e^{*}|','fontsize',20)
 legend({'|e(t) - e^{*}|','exp. decay'},'location','eastoutside','fontsize',14)
 xlabel('Time','fontsize',14)
 ylabel('|e(t) - e^{*}|','fontsize',14)
 xlim([0 1000])
-ylim([0 0.6])
+ylim([0 0.8])
 hold off
 
 figure(6)
 plot(t,abs(p-(s_0+c_0+p_0)*ones(length(e),1)),'linewidth',0.8,'marker','*','linestyle','-')
 hold on
-plot(t,max([s_0 e_0 c_0 p_0]).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
+plot(t,1*(s_0+e_0+c_0+p_0).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
 title('Exponential decay of |p(t) - p^{*}|','fontsize',20)
 legend({'|p(t) - p^{*}|','exp. decay'},'location','eastoutside','fontsize',14)
 xlabel('Time','fontsize',14)
 ylabel('|p(t) - p^{*}|','fontsize',14)
 xlim([0 1000])
-ylim([0 0.6])
+ylim([0 0.8])
+hold off
+
+figure(7)
+plot(t,s,'linewidth',0.8,'marker','*','linestyle','-')
+hold on
+plot(t,1*(s_0+e_0+c_0+p_0).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
+title('Exponential decay of |s(t) - 0| (Zoom in)','fontsize',20)
+legend({'s(t)','exp. decay'},'location','eastoutside','fontsize',14)
+xlabel('Time','fontsize',14)
+ylabel('s(t)','fontsize',14)
+xlim([2000 10000])
+ylim([0 5E-08])
+hold off
+
+figure(8)
+plot(t,c,'linewidth',0.8,'marker','*','linestyle','-')
+hold on
+plot(t,1*(s_0+e_0+c_0+p_0).*exp(-abs(lambda).*t),'linewidth',0.8,'linestyle','-')
+title('Exponential decay of |c(t) - 0| (Zoom in)','fontsize',20)
+legend({'c(t)','exp. decay'},'location','eastoutside','fontsize',14)
+xlabel('Time','fontsize',14)
+ylabel('c(t)','fontsize',14)
+xlim([2000 10000])
+ylim([0 1E-08])
+hold off
+
+figure(9)
+plot(t,c./s,'color','blue','linewidth',0.8,'linestyle','-')
+hold on
+plot(t,A*ones(length(t),1),'color','black','linewidth',0.8,'linestyle','--')
+hold off
+legend({'c(t)/s(t)','Limit A'},'location','eastoutside','fontsize',14)
+title('Convergence of c(t)/s(t) to A','fontsize',20)
+xlabel('Time','fontsize',14)
+ylabel('c(t)/s(t)','fontsize',14)
+hold off
+
+figure(10)
+plot(t(2:1:end),(e(2:1:end).*s(2:1:end))./c(2:1:end),'color','blue','linewidth',0.8,'linestyle','-')
+hold on
+plot(t(2:1:end),((e_0+c_0)/A)*ones(length(t(2:1:end)),1),'color','black','linewidth',0.8,'linestyle','--')
+hold off
+legend({'(e(t)*s(t))/c(t)','Limit'},'location','eastoutside','fontsize',14)
+title('Convergence of (e(t)*s(t))/c(t)','fontsize',20)
+xlabel('Time','fontsize',14)
+ylabel('(e(t)*s(t))/c(t)','fontsize',14)
 hold off
